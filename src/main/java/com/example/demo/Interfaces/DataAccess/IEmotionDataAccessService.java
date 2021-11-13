0910f8ac30bdbe5275527fd4e1e2f9e462697caf
Prelipcean Cristian <prelipceancristian100@gmail.com>
2021-11-13 20:56:23 +0200

    Default controller for the Emotion entity

--- src/main/java/com/example/demo/Interfaces/DataAccess/IEmotionDataAccessService.java
+++ src/main/java/com/example/demo/Interfaces/DataAccess/IEmotionDataAccessService.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public interface IEmotionDataAccessService {
     List<Emotion> RetrieveEmotions();
+
     Emotion SearchEmotion(int id);
-    void CreateEmotion(Emotion emotion);
-    void UpdateEmotion(Emotion emotion);
+
+    void CreateEmotion(String emotionName, int emotionCategoryID, int emotionDescriptionID);
+
+    void UpdateEmotion(int emotionID, String emotionName, int emotionCategoryID, int emotionDescriptionID);
+
     void DeleteEmotion(int id);
 }
